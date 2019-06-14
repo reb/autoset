2df2cc0d63cb0d775155c199bcc595bec4fcce18
reb <reboumans@gmail.com>
2019-06-14 14:19:51 +0200

    return bounding box

--- generator.py
+++ generator.py
@@ -27,22 +27,38 @@ def transform_image(img):
   width, height = img.size
   quarterWidth = int(width / 4)
   quarterHeight = int(height / 4)
-  coeffs = find_coeffs(
-    [
+  goal = [
       (random.randint(0, quarterWidth), random.randint(0, quarterHeight)),
       (random.randint(width - quarterWidth, width), random.randint(0, quarterHeight)),
       (random.randint(width - quarterWidth, width), random.randint(height - quarterHeight, height)),
-      (random.randint(0, quarterWidth), random.randint(height - quarterHeight, height))],
+      (random.randint(0, quarterWidth), random.randint(height - quarterHeight, height))
+  ]
+
+  coeffs = find_coeffs(
+      goal,
     [
       (0, 0),
       (width, 0),
       (width, height),
-      (0, height)])
+      (0, height)
+    ]
+  )
 
   perspectified = img.transform((width, height), Image.PERSPECTIVE, coeffs, Image.BICUBIC)
   cardSize = 200
-  resized = perspectified.resize((cardSize, int(cardSize * perspectified.size[1] / perspectified.size[0])), Image.BICUBIC)
-  return resized
+  # resized = perspectified.resize((cardSize, int(cardSize * perspectified.size[1] / perspectified.size[0])), Image.BICUBIC)
+  return perspectified, goal
+
+
+def convert_to_relative(boundingBox, location, imageSize):
+  return [convert_point(p, location, imageSize) for p in boundingBox]
+
+
+def convert_point(point, location, imageSize):
+  x, y = point
+  locationX, locationY = location
+  width, height = imageSize
+  return ((x + locationX) / width, (y + locationY) / height)
 
 
 def render_background(img):
@@ -72,7 +88,7 @@ def generate(id, cardIndex, csv_file):
   # for y in range(3):
   #   for x in range(3):
 
-  cardToPlace = transform_image(card)
+  cardToPlace, boundingBox = transform_image(card)
 
   output.paste(cardToPlace, (random.randint(0, SIZE - cardToPlace.size[0]), random.randint(0, SIZE - cardToPlace.size[1])), cardToPlace)
 
