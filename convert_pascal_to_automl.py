65746b7cfde9098673f57625907d546910d3ff4d
reb <reboumans@gmail.com>
2019-06-15 20:29:16 +0200

    Enable converting entire folders of Pascal xml files

--- convert_pascal_to_automl.py
+++ convert_pascal_to_automl.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import sys
+import glob
 
 
 def get_float(node, path):
@@ -10,14 +11,20 @@ def get_text(node, path):
     return node.findall(path)[0].text
 
 
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
-filename = get_text(root, "./filename")
+def convert_folder(folder_name, csv_file):
+    file_names = [f for f in glob.glob(f'{folder_name}*.xml')]
+    for file_name in file_names:
+        convert_file(file_name, csv_file)
 
-width = get_float(root, "./size/width")
-height = get_float(root, "./size/height")
 
-with open('bbs.csv', 'w') as csv_file:
+def convert_file(file_name, csv_file):
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    filename = get_text(root, "./filename")
+
+    width = get_float(root, "./size/width")
+    height = get_float(root, "./size/height")
+
     tags = root.findall("./object")
     for tag in tags:
         xmin = get_float(tag, "./bndbox/xmin")
@@ -30,3 +37,8 @@ with open('bbs.csv', 'w') as csv_file:
         bottom_right = f'{(xmax / width)},{(ymin / height)}'
 
         csv_file.write(f'UNASSIGNED,gs://autoset-vcm/annotated-photos/images/{filename},{name},{top_left},,,{bottom_right},,\n')
+
+if __name__ == "__main__":
+    with open('bbs.csv', 'w') as csv_file:
+        convert_folder(sys.argv[1], csv_file)
+
