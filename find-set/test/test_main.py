8067e519d418792101f4232cbfdcbcf71a7642be
reb <reboumans@gmail.com>
2019-10-02 14:22:27 -0400

    Test the find_set function

--- find-set/test/test_main.py
+++ find-set/test/test_main.py
@@ -17,6 +17,25 @@ class IsSet(unittest.TestCase):
         self.assertFalse(is_set("1RED", "2BHW", "3BWP"))
 
 
+class FindSet(unittest.TestCase):
+    def test_correct_set(self):
+        cards = [
+            {'name': '1ED', 'bounding_box': [0.373840332, 0.728388488, 0.621475697, 0.892044723]},
+            {'name': '1HD', 'bounding_box': [0.385559499, 0.440285, 0.633674562, 0.60816741]},
+            {'name': '1FD', 'bounding_box': [0.343466312, 0.138860583, 0.589227438, 0.328077763]},
+            {'name': '2ED', 'bounding_box': [0.373840332, 0.728388488, 0.621475697, 0.892044723]},
+            {'name': '1HW', 'bounding_box': [0.385559499, 0.440285, 0.633674562, 0.60816741]},
+            {'name': '3FW', 'bounding_box': [0.343466312, 0.138860583, 0.589227438, 0.328077763]},
+        ]
+
+        found_set = (
+            {'name': '1ED', 'bounding_box': [0.373840332, 0.728388488, 0.621475697, 0.892044723]},
+            {'name': '1HD', 'bounding_box': [0.385559499, 0.440285, 0.633674562, 0.60816741]},
+            {'name': '1FD', 'bounding_box': [0.343466312, 0.138860583, 0.589227438, 0.328077763]},
+        )
+        self.assertEqual(find_set(cards), found_set)
+
+
 if __name__ == '__main__':
     unittest.main()
 
