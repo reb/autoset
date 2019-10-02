963dec4789869f4317e1fc50d224df615100abb3
reb <reboumans@gmail.com>
2019-10-02 14:18:53 -0400

    Prevent the storage module from initializing anything if only imported

--- find-set/function/storage.py
+++ find-set/function/storage.py
@@ -2,11 +2,15 @@ import os
 from datetime import datetime
 from google.cloud import storage
 
-STORAGE_CLIENT = storage.Client()
+STORAGE_CLIENT = None
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
 
 def upload(file_data, hash):
+    global STORAGE_CLIENT
+    if STORAGE_CLIENT is None:
+        STORAGE_CLIENT = storage.Client()
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H%M%S.%f")
 
     bucket = STORAGE_CLIENT.get_bucket(BUCKET_NAME)
